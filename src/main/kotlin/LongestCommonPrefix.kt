de45d7f64f500e29f0a4f4e7ce92da2c070b926d
dmitry <piervov00@gmail.com>
2023-01-11 12:18:35 +0300

    Longest Common Prefix

---
+++ src/main/kotlin/LongestCommonPrefix.kt
@@ -0,0 +1,11 @@
+class LongestCommonPrefix {
+    fun longestCommonPrefix(strs : Array<String>): String {
+        if (strs.isEmpty()) return ""
+        var prefix = strs[0]
+        for (i in 1 until strs.size)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length-1)
+            }
+        return prefix
+    }
+}
